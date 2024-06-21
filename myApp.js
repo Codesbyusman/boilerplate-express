f6491190b12a91bdefc7d3f6037a6e30e77947e7
Muhammad Usman  Shahid <usman.shahid@emumba.com>
2024-06-21 06:59:59 +0000

    json_response

--- myApp.js
+++ myApp.js
@@ -21,11 +21,16 @@ absolutePath2 = __dirname + "/public"
 
 app.use(
    "/public",  express.static(absolutePath2)
-  )
+)
+
+function get_data(req, res){
+    res.json({
+        "message" : "Hello json"
+    })
+}
 
 app.get('/' , file)
-
-
+app.get('/json', get_data)
 
 
 
